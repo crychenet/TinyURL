17e9de5848b4dc3d5610aa01b41328d38652fb35
crychenet <137490656+crychenet@users.noreply.github.com>
2025-04-01 20:24:30 +0300

    Update README.md

--- init_db.py
+++ init_db.py
@@ -7,7 +7,6 @@ from models import User
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    print("Таблицы успешно созданы")
 
 
 if __name__ == "__main__":
